f01504077036c002a8cb7f372413d4d43b5f567e
AlexNuzhdov <alexnuzhdov90@gmail.com>
2022-08-21 21:26:38 +0300

    add refs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-refs = {
+const refs = {
   input: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),  
